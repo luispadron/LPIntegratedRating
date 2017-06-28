7aa70aae3ac176f804010385bd3bf95e751dfe60
Luis Padron <luispadronn@gmail.com>
2017-06-28 18:13:55 -0400

    Add license text to files

--- LPIntegratedRating/LPRatingView.swift
+++ LPIntegratedRating/LPRatingView.swift
@@ -2,8 +2,25 @@
 //  LPRatingView.swift
 //  LPIntegratedRating
 //
-//  Created by Luis Padron on 6/27/17.
-//  Copyright © 2017 Luis Padron. All rights reserved.
+//  Copyright (c) 2017 Luis Padron
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is furnished
+//  to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+//  FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+//  OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 import UIKit
@@ -11,6 +28,7 @@ import UIKit
 internal extension UIColor {
     static let defaultColor: UIColor = UIColor(red: 0.310, green: 0.325, blue: 0.616, alpha: 1.00)
 }
+
 
 open class LPRatingView: UIView {
     
