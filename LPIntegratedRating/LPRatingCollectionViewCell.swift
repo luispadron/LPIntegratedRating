2c909ca5304233d59542a4bb415f89ca447dbcb2
Luis Padron <luispadronn@gmail.com>
2017-06-28 15:00:12 -0400

    Add tableview cell subclass for easy use

---
+++ LPIntegratedRating/LPRatingCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  LPRatingCollectionViewCell.swift
+//  LPIntegratedRating
+//
+//  Created by Luis Padron on 6/28/17.
+//  Copyright © 2017 Luis Padron. All rights reserved.
+//
+
+import Foundation
